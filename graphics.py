5b9bd5ee47cc37101b90af8f975f84b4c148380d
olu <oluegbiaye@gmail.com>
2025-08-02 14:57:17 +0200

    Add line drawing support using Point and Line classes

--- graphics.py
+++ graphics.py
@@ -1,24 +1,29 @@
-import tkinter as tk
-from tkinter import Tk
+from tkinter import Canvas, Tk
+
+from draw_lines import Line
 
 
 class Window:
     def __init__(self, width: int, height: int):
         self.__root = Tk()
         self.__root.title("Maze solver")
-        self.__canves = tk.Canvas(self.__root, width=width, height=height, bg="white")
+        self.__canves = Canvas(self.__root, width=width, height=height, bg="white")
         self.__canves.pack(fill="both", expand=1)
         self.__is_runnig = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
-        self.__root.update()
         self.__root.update_idletasks()
+        self.__root.update()
 
     def wait_for_close(self):
         self.__is_runnig = True
         while self.__is_runnig is True:
             self.redraw()
+        print("window closed...")
 
     def close(self):
-        self.__s_runnig = False
+        self.__is_runnig = False
+
+    def draw_line(self, line: Line, fill_color="black"):
+        line.draw(self.__canves, fill_color)
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+from draw_lines import Line, Point
 from graphics import Window
 
 
 def main():
     win = Window(800, 600)
+    p1 = Point(100, 100)
+    p2 = Point(400, 300)
+
+    line = Line(p1, p2)
+    win.draw_line(line, "black")
     win.wait_for_close()
 
 
