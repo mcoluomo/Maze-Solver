d44a1238dea69dd330d754367bf7084dbd41259c
olu <oluegbiaye@gmail.com>
2025-08-03 21:29:23 +0200

    Add Cell class and cell drawing logic to graphics module

--- graphics.py
+++ graphics.py
@@ -1,6 +1,6 @@
 from tkinter import Canvas, Tk
 
-from draw_lines import Line
+from draw_lines import Line, Point
 
 
 class Window:
@@ -27,3 +27,66 @@ class Window:
 
     def draw_line(self, line: Line, fill_color="black"):
         line.draw(self.__canves, fill_color)
+
+
+class Cell:
+    def __init__(self, win: Window) -> None:
+        self.has_left_wall = True
+        self.has_right_wall = True
+
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+
+        self.__x1 = -1
+        self.__x2 = -1
+
+        self.__y1 = -1
+        self.__y2 = -1
+        self.__win = win
+
+    def draw(self, x1, x2, y1, y2):
+        if self.__win is None:
+            return
+        self.__x1 = x1
+        self.__x2 = x2
+        self.__y1 = y1
+        self.__y2 = y2
+        if self.has_left_wall is True:
+            p1 = Point(x1, x2)
+            p2 = Point(y1, y2)
+            line = Line(p1, p2)
+            self.__win.draw_line(line, "black")
+
+        if self.has_top_wall is True:
+            p1 = Point(x1, x2)
+            p2 = Point(y1, y2)
+            line = Line(p1, p2)
+            self.__win.draw_line(line, "black")
+
+        if self.has_right_wall is True:
+            p1 = Point(x1, x2)
+            p2 = Point(y1, y2)
+            line = Line(p1, p2)
+            self.__win.draw_line(line, "black")
+
+        if self.has_top_wall is True:
+            p1 = Point(x1, x2)
+            p2 = Point(y1, y2)
+            line = Line(p1, p2)
+            self.__win.draw_line(line, "black")
+
+    def draw_move(self, to_cell, undo=False):
+        half_length = abs(self.__x2 - self.__x1) // 2
+        x_center = half_length + self.__x1
+        y_center = half_length + self.__y1
+
+        half_length2 = abs(to_cell.__x2 - to_cell.__x1) // 2
+        x_center2 = half_length2 + to_cell.__x1
+        y_center2 = half_length2 + to_cell.__y1
+
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+        self.__win.draw_line(line, fill_color)
--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-from draw_lines import Line, Point
-from graphics import Window
+from graphics import Cell, Window
 
 
 def main():
     win = Window(800, 600)
-    p1 = Point(100, 100)
-    p2 = Point(400, 300)
 
-    line = Line(p1, p2)
-    win.draw_line(line, "black")
+    c1 = Cell(win)
+    c1.has_right_wall = False
+    c1.draw(50, 50, 100, 100)
+
+    c2 = Cell(win)
+    c2.has_left_wall = False
+    c2.has_bottom_wall = False
+    c2.draw(100, 50, 150, 100)
+
+    c1.draw_move(c2)
+
+    c3 = Cell(win)
+    c3.has_top_wall = False
+    c3.has_right_wall = False
+    c3.draw(100, 100, 150, 150)
+
+    c2.draw_move(c3)
+
+    c4 = Cell(win)
+    c4.has_left_wall = False
+    c4.draw(150, 100, 200, 150)
+
+    c3.draw_move(c4, True)
+
     win.wait_for_close()
 
 
